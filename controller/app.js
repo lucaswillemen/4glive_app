b6b11acdcddb2780f2fdfa9730578b7e8fe5ede4
Lucas Willemen <lucaswillemen@gmail.com>
2017-03-04 06:19:44 -0300

    'a

--- controller/app.js
+++ controller/app.js
@@ -33,6 +33,6 @@ app.controller('Main', function($scope, $rootScope, $state, $http) {
 	    $("body").addClass("loading-overlay-showing")
 	})
 	$scope.open_link_update = function(){
-        var ref = cordova.InAppBrowser.open('market://details?id=glive.com.br', '_system', 'location=yes');
+        navigator.app.loadUrl('https://play.google.com/store/apps/details?id=glive.com.br');
 	}
 });
