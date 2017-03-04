c6f6775b5093561930ba82143b0b1f99b1240a21
Lucas Willemen <lucaswillemen@gmail.com>
2017-03-04 04:55:42 -0300

    'a

--- controller/state.js
+++ controller/state.js
@@ -92,5 +92,4 @@ app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
         })
         $urlRouterProvider.otherwise("/login")
 
-        $locationProvider.html5Mode(true);
 })
